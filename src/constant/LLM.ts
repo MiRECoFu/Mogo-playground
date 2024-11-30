200e99de2705cf9b3a03e1f1a568c9ef05bf7a01
MiRECoFu <1027066037@qq.com>
2024-11-30 11:10:18 +0800

    feature: add attack model llm rewrite

--- src/constant/LLM.ts
+++ src/constant/LLM.ts
@@ -1,6 +1,6 @@
 export const enhancePrompt = `你现在是一个人体行为机器学习专家。需要为一个用HumanML3D 数据集训练的文字生成动作序列的模型编写prompt。
 你需要将抽象的动作描述直接用英文描述成具体的动作，需要细致到具体的肢体行为，动作方向等。请你直接输出具体描述，限制在一句话，25词以内。
-不要有具体的和其他物体交互，只描述人体动作。如果输入的prompt 是具体的动作描述并且是英文，请直接返回原始 prompt输入不要修改。作为参考，原始数据集中只有日常行为动作、拳击动作、街舞类型。\n
+不要有具体的和其他物体交互，只描述人体动作。如果输入的prompt 是具体的动作描述并且是英文，请直接返回原始 prompt输入不要修改。\n
 训练集的 prompt 举例如下：\n
 person walking with their arms swinging back to front and walking in a general circle\n
 a person is standing and then makes a stomping gesture\n
@@ -13,8 +13,10 @@ This person kicks with his right leg then jabs several times.\n
 a person lifting both arms together in front of them and then lift them back down\n
 a man walks up and down from either stairs, rocks, or some unlevel terrain requiring a step.\n
 a person with dance moves\n
-注意：1.不要写具体的人物,动作主体均为 a man、a person.因为训练集中没有具体的人物，比如：剑客、魔法师、士兵等，你需要尽可能通过语言描述肢体动作来表达他的形象。
-2.不要写携带的物体，因为训练集中没有具体的物体，比如：剑、刀、枪等，你需要尽可能通过语言描述肢体动作来表达。
+The character swings their fist to the right and stands up at a moderate pace from a crouched position with a slight stagger.
+The character swings their attack with their right hand without charging, firmly squatting and holding the weapon at their waist, and then strikes from left to right.
+The character uses a wedge to perform a basic movement, swiftly running forward with lightness. They start by kneeling on their left knee, with the left arm hanging down and the right hand placed to the side of the shoulder. After pushing off with their left foot, they rotate in the air to face backward, land with their right foot, and return to the running posture.
+注意：1.不要写具体的人物,动作主体均为 a man、a person.
 3.你的描述要尽可能的用词简单清晰，不要使用复杂词汇。
 4.尽可能模仿我上面给你的 prompt例子的用词方式描述。\n
 举例：
